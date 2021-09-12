060b2af49c94e6c8406581703c6ec4ea2e452c3d
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-09-12 08:16:08 +0100

    add missing lines to previous merge

--- R/main.R
+++ R/main.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 library(tidyverse)
 library(rvest)
+library(lubridate)
+library(plotly)
 
 source("R/functions/discover_links.R")
 source("R/functions/download_data_from_url.R")
@@ -8,7 +10,10 @@ source("R/functions/download_msds_data.R")
 source("R/functions/sort_data_into_subfolders.R")
 source("R/functions/move_file_into_subfolder.R")
 source("R/functions/discover_data_type.R")
+source("R/functions/combine_files_to_dataframe.R")
 source("R/functions/move_file.R")
+source("R/functions/get_measures.R")
+source("R/functions/plots/measure_comparative_plot.R")
 
 # Download data from the NHSDigital url.
 # As of Sept 2021, this function will download ~725MB of data
@@ -22,11 +27,11 @@ sort_data_into_subfolders()
 
 # There are 3 main data source files:  measures, data, and dq
 # MEASURES #
-  #read measures data into a dataframe, including any background data cleaning
-  dtf <- get_measures() #OK 401k rows
+#read measures data into a dataframe, including any background data cleaning
+dtf <- get_measures() #OK 401k rows
 
-  #quick plot of all providers data for a given measure
-  measure_comparative_plot(dtf, "CQIMPreterm", "RX1")
+#quick plot of all providers data for a given measure
+measure_comparative_plot(dtf, "CQIMPreterm", "RX1")
 
 # DATA #
   # Coming soon

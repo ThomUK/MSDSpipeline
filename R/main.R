c8903e6260489cbe654608c42e990739ca4286e8
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-09-12 23:18:19 +0100

    source all files using a single sub-file

--- R/main.R
+++ R/main.R
@@ -4,18 +4,7 @@ library(rvest)
 library(lubridate)
 library(plotly)
 
-source("R/functions/discover_links.R")
-source("R/functions/download_data_from_url.R")
-source("R/functions/download_msds_data.R")
-source("R/functions/sort_data_into_subfolders.R")
-source("R/functions/move_file_into_subfolder.R")
-source("R/functions/discover_data_type.R")
-source("R/functions/combine_files_to_dataframe.R")
-source("R/functions/move_file.R")
-source("R/functions/get_measures.R")
-source("R/functions/get_data.R")
-source("R/functions/plots/measure_comparative_plot.R")
-source("R/functions/plots/data_comparative_plot.R")
+source("R/source/source_all_files.R")
 
 # Download data from the NHSDigital url.
 # As of Sept 2021, this function will download ~725MB of data

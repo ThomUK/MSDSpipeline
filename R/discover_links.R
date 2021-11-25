c2475ae5ec41eddf97712b48d801e867a5c76fb6
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-11-25 19:33:52 +0000

    move functions into main R folder

--- R/discover_links.R
+++ R/discover_links.R
@@ -1,18 +1,26 @@
+#' Discover the links (a tags) at a given web page URL
+#'
+#' @param page_url A character string for the web page URL
+#'
+#' @return A dataframe of links and page URLs
+#'
+#' @noRd
+#'
 discover_links <- function(page_url){
-  
+
   #read the page
   html <- read_html(page_url)
-  
-  #read the URLs  
+
+  #read the URLs
   url_ <- html %>%
     rvest::html_nodes("a") %>%
     rvest::html_attr("href")
-  
+
   # Extract the link text
   link_ <- html %>%
     rvest::html_nodes("a") %>%
     rvest::html_text()
-  
+
   return(tibble(link = link_, url = url_))
-  
+
 }

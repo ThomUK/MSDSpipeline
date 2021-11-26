810adeaa333f38d6a3ab04c69d5be76010c4f065
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-11-26 23:00:41 +0000

    simplify path logic

--- R/start_download.R
+++ R/start_download.R
@@ -13,11 +13,8 @@ start_download <- function(url, destination_folder){
   destination_filename <- basename(url)
 
   #check the folder exists, and if not, create it
-  todir <- paste0(getwd(), "/", destination_folder) # full file path
-  if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive=TRUE)
-
-  #TODO fix the download location (folder is created, but files go into parent folder)
+  if (!isTRUE(file.info(destination_folder)$isdir)) dir.create(destination_folder, recursive=TRUE)
 
   #do the download
-  download.file(url, paste0(todir, "/", destination_filename), mode = "wb")
+  download.file(url, file.path(destination_folder, destination_filename), mode = "wb")
 }

ca3358e9981096c66a0ea7543bab046ca9437b4d
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-11-27 19:43:42 +0000

    modify test

--- tests/testthat/test-build_data_path.R
+++ tests/testthat/test-build_data_path.R
@@ -1,17 +1,8 @@
 test_that("destination is a character vector", {
 
   expect_error(
-    build_data_path(destination = FALSE, relative_path = TRUE),
+    build_data_path(destination = FALSE),
     "build_data_path: destination argument must be a character vector."
-  )
-
-})
-
-test_that("destination is a character vector", {
-
-  expect_error(
-    build_data_path(destination = "a/path", relative_path = 1),
-    "build_data_path: relative_path argument must be logical."
   )
 
 })
@@ -19,17 +10,26 @@ test_that("destination is a character vector", {
 test_that("a relative path is returned", {
 
   expect_equal(
-    build_data_path("a/relative/path", relative_path = TRUE),
+    build_data_path("a/relative/path"),
     paste0(getwd(), "/a/relative/path")
   )
 
 })
 
-test_that("an absolute path can be returned", {
+test_that("a drive-based absolute path can be returned", {
 
   expect_equal(
-    build_data_path("C:/an/absolute/path", relative_path = FALSE),
+    build_data_path("C:/an/absolute/path"),
     "C:/an/absolute/path"
+  )
+
+})
+
+test_that("a network-based absolute path can be returned", {
+
+  expect_equal(
+    build_data_path("\\\\an\\absolute\\path"),
+    "//an/absolute/path"
   )
 
 })

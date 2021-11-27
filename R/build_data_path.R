8812f428909847a79ffb817a49f2dc388005cdcd
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-11-27 19:44:13 +0000

    simplify wrapping function

--- R/build_data_path.R
+++ R/build_data_path.R
@@ -1,19 +1,10 @@
-build_data_path <- function(destination, relative_path){
+build_data_path <- function(destination){
 
   assertthat::assert_that(
     inherits(destination, "character"),
     msg = "build_data_path: destination argument must be a character vector."
   )
 
-  assertthat::assert_that(
-    inherits(relative_path, "logical"),
-    msg = "build_data_path: relative_path argument must be logical."
-  )
-
-  if(relative_path){
-    file.path(getwd(), destination)
-  } else {
-    file.path(destination)
-  }
+  R.utils::getAbsolutePath(destination)
 
 }
